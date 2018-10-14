fd4252bf72b963b8fa4221b14caeaa8d50c39b37
Rodrigo de Leon <rodri230998@gmail.com>
2018-10-14 01:53:02 -0500

    arrays

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 print("hello world")
 
-for x in "hello world":
-    print (x)
+for x in "hello sudo":
+	print (x)
+	if x == "o":
+		break
+numbers = []
+for x in range(0, 1000, 10):
+	numbers.append(x)
+
+for x in range(0, len(numbers), 1):
+	print(numbers[x])
